a50187961cca739d427d999af5a90f3fe5d85b75
Doug Wiegley <doug@wiegley.com>
2013-09-09 17:11:33 -0600

    initial generate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestLdap::Application.config.secret_token = '298588f72ace87ec07d363886949a748f842cc6fdf5ddb18873de28834228fd82dc9c46a84ebc7fb54d9a15524b8e2550edcfb722356fa6ff5336c9326b5b98a'
